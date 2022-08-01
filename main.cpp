95c345842ccb92d002c1f28d6045349a211ee601
TUMO Labs Student <tumolabsstudent@TUMOs-iMac.local>
2022-08-01 21:00:36 +0400

    start cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Am");
+	int sum = 34;
+	short days = 7;
+	long multiply  = 3666666666;
+	bool isNan = false;
+	double num = 3.445;
+	char simbal = 'h';
+	std::cout << num <<"\"barev"<< "\n";
+    cout <<"\t\t" << simbal<< "\n" ;
+	return(0);
+}
