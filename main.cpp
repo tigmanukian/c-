4341d5644cdf64e71d9ca1eb0f33bd9fe4ae58d2
TUMO Labs Student <tumolabsstudent@TUMOs-iMac.local>
2022-08-12 20:18:02 +0400

    unic array

--- main.cpp
+++ main.cpp
@@ -1,24 +1,135 @@
 #include <iostream>
-
+#include <ctime>
 using namespace std;
 
 int main()
 {
-	int width , height;
+	setlocale(LC_ALL, "am");
 
-	cout << "please enter orthogonal height" << "\n";
-	cin >> height;
-	cout << " please enter orthogonal width" << "\n";
-	cin >> width;
-	for (int i = 0; i < height; i++)
+	srand(time(NULL));
+
+	int const SIZE = 10;
+	int arr[SIZE];
+	bool alreadyThere;
+
+	for (int i = 0; i < SIZE; )
 	{
-		for (int j = 0; j < width ; j++)
-		{	
-			cout << "*" ;
+		alreadyThere = false;
+		int newRandomValue = rand() % 20;
+		
+		for(int j = 0 ; j < i; j++){
+			if (arr[j] == newRandomValue )
+			{
+				alreadyThere = true;
+				break;
+			}
 		}
-		cout  << endl;
+		if(!alreadyThere )
+		{
+			arr[i] = newRandomValue;
+			i++;
+		}
 	}
-}  
+
+
+	for(int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+// 	setlocale(LC_ALL, "am");
+// 	// const  int size = 5;
+
+// 	int  arr[6] = {5,8,34,87,2,3}; 
+
+// 	double desimolNum = 23.5;
+// 	bool isRight = false;
+// 	string addText = "I am right people ";
+// 	char  symbol = '\\';
+
+// 	for (int  i = 0; i < sizeof(arr)/ sizeof(arr[0]) ; i++)
+// 	{
+// 		if(arr[i] < 10)
+// 		{
+// 			cout << arr[i] << endl; 
+// 		}
+// 		// cout << arr[i] << endl; 
+// 	}
+  
+// 	cout << sizeof(arr) << "   sizeof"<< endl;
+
+	
+// }
+
+
+
+
+
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+// 	int width , height;
+
+// 	cout << "please enter orthogonal height" << "\n";
+// 	cin >> height;
+// 	cout << " please enter orthogonal width" << "\n";
+// 	cin >> width;
+// 	for (int i = 0; i < height; i++)
+// 	{
+// 		for (int j = 0; j < width ; j++)
+// 		{	
+// 			cout << "*" ;
+// 		}
+// 		cout  << endl;
+// 	}
+// }  
 
 
 
